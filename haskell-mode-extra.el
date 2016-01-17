3af1c6662637394f6672c46e1f591ff66c310de1
HVic <Snail Mail>
2016-01-16 22:42:01 -0500

    initial commit

---
+++ haskell-mode-extra.el
@@ -0,0 +1,37 @@
+;; haskell-mode convenience functions
+;;
+;; trims a line and sends it to the interactive haskell process
+;; useful for testing out code in a monadic block interactively
+;;
+;; author: HVic
+;; install: add to .emacs or .spacemacs (in user-config)
+;; (define-key haskell-mode-map (kbd "<C-return>") 'haskell-line-to-repl)
+
+;; trim code: http://ergoemacs.org/emacs/modernization_elisp_lib_problem.html
+(defun s-trim-left (s)
+  (if (string-match "\\`[ \t\n\r]+" s)
+      (replace-match "" t t s)
+    s))
+
+(defun s-trim-right (s)
+  (if (string-match "[ \t\n\r]+\\'" s)
+      (replace-match "" t t s)
+    s))
+
+(defun s-trim (s)
+  (s-trim-left (s-trim-right s)))
+
+(defun haskell-line-to-repl ()
+  (interactive)
+  (let ((trimmed (s-trim (thing-at-point 'line))))
+    (haskell-interactive-switch)
+    (goto-char (point-max))
+    (insert trimmed)
+    (funcall (key-binding (kbd "RET"))) 
+    (goto-char (point-max))
+    (haskell-interactive-switch-back)
+    (forward-line 1)
+    )
+  )
+:0
+j
